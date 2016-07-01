9f44bb2d7213a20e0c01cfd928881139c684f453
Justin Lee <justinjlee99@gmail.com>
2016-07-01 14:35:33 -0700

    v1 finished

---
+++ crammunity/ClassViewCell.swift
@@ -0,0 +1,13 @@
+//
+//  ClassViewCell.swift
+//  crammunity
+//
+//  Created by Clara Hwang on 6/29/16.
+//  Copyright © 2016 orctech. All rights reserved.
+//
+
+import Cocoa
+
+class ClassViewCell: UITableViewCell {
+
+}
