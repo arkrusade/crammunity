ac1414163adfb496313744f17918f6412369aef8
Justin Lee (dad) <justinjlee99@gmail.com>
2017-02-10 23:58:10 -0500

    resolve swift 3, errors, bridging

---
+++ crammunity/crammunity-Bridging-Header.h
@@ -0,0 +1,13 @@
+//
+//  crammunity-Bridging-Header.h
+//  crammunity
+//
+//  Created by Justin Lee on 2/10/17.
+//  Copyright © 2017 orctech. All rights reserved.
+//
+
+#ifndef crammunity_Bridging_Header_h
+#define crammunity_Bridging_Header_h
+
+
+#endif /* crammunity_Bridging_Header_h */
